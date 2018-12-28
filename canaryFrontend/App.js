23a40a8127f70e474a958072f5b080343bb8ac98
David Batista de Luna <ddavidr569@gmail.com>
2018-12-28 20:37:08 -0300

    [Create]Creating the project frontend

---
+++ canaryFrontend/App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+//type Props = {};
+export default class App extends Component/* <Props> */ {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Projetinho</Text>
+        <Text style={styles.instructions}>Topper, vale do silicio é nosso</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
