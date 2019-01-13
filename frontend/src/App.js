602a75be8d5467bebe5e47bbdba6f82423417fee
Gabriel Barros <gabriel.sb199@gmail.com>
2019-01-13 18:58:31 -0300

    Alterado componente personalizado para o DrawerNavigator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Image, Container, Content, ScrollView, SafeAreaView, AppContainer } from "react-native";
-import { createStackNavigator, createDrawerNavigator, createAppContainer } from "react-navigation";
+import { Platform, StyleSheet, Text, View, Image, Container, Content, ScrollView, TouchableOpacity, AppContainer } from "react-native";
+import { createStackNavigator, createDrawerNavigator, createAppContainer, NavigationActions } from "react-navigation";
 import Autentication from "./screens/Authentication.js";
 import Home from "./screens/Home.js";
+import Settings from "./screens/Settings.js";
+
+import SimpleIcon from "react-native-vector-icons/SimpleLineIcons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Octicons from "react-native-vector-icons/Octicons";
+
 
 
 const CustomDrawerComponent = (props) => (
     <View style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+        <View>
         <Image source={require("../assets/imgs/logo.png")}
-        style={{}}/>
-        <Text>Text</Text>
-        <Text>Text</Text>
-        <Text>Text</Text>
-        <Text>Text</Text>
-        <Text>Text</Text>
-        <Text>Text</Text>
+        style={styles.logo}/>
+        </View>
+        <Text style={[styles.border, styles.margin]}></Text>
+
+        <TouchableOpacity style={styles.itemContainer} onPress={()=>{props.navigation.navigate("Home")}}>
+            <SimpleIcon name="home" color="white" size={20} style={styles.icon}/> 
+            <Text style={styles.item} >Home</Text>
+        </TouchableOpacity>
+        
+        <View style={styles.itemContainer}>
+            <AntDesign name="yuque" color="white" size={20} style={styles.icon}/>
+            <Text style={styles.item}>Canarios</Text>
+        </View>
+
+        <Text style={[styles.border, styles.margin]}></Text>
+
+        <TouchableOpacity style={styles.itemContainer}>
+            <MaterialCommunityIcons name="map-marker-outline" color="white" size={20} style={styles.icon}/>
+            <Text style={styles.item}>Mapa</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.itemContainer} onPress={()=>{props.navigation.navigate("Settings")}}>
+            <AntDesign name="user" color="white" size={20} style={styles.icon}/>
+            <Text style={[styles.item, {marginBottom: 220}]}>Perfil</Text>
+        </TouchableOpacity>
+       
+        <Text style={styles.sistema}>Sistema</Text>
+        <TouchableOpacity style={styles.itemContainer}>
+            <SimpleIcon name="exclamation" color="white" size={20} style={{marginLeft:27}}/>
+            <Text style={styles.item}>Sobre</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.itemContainer}>
+            <Octicons name="trashcan" color="#f1c484" size={20} style={styles.icon}/>
+            <Text style={[styles.item, styles.delete]}>Excluir Conta</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.border}></Text>
+        
+        <TouchableOpacity style={styles.itemContainer}>
+            <SimpleIcon name="logout" color="white" size={20} style={{marginLeft:25}}/>
+            <Text style={styles.item}>Sair</Text>
+        </TouchableOpacity>
+        
     </View>
 );
 
 
+/*
 
+dove
+crow
+*/
 
 const AppDrawerNavigator = createDrawerNavigator({
     Home: { screen: Home },
+    Settings: { screen: Settings},
 },
 {
     initialRouteName: "Home",
@@ -46,10 +96,53 @@ export default createAppContainer(AppStackNavigator);
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1,/*
         justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#5C6BC0"
+        alignItems: "center",*/
+        backgroundColor: "#5C6BC0",
+        color: "white"
     },
+    logo: {
+        width: 150,
+        height: 80,
+        marginTop: 15,
+        marginHorizontal: 65,
+        borderBottomWidth: 50,
+        borderColor: "white",
+    },
+    item: {
+        color: "white", 
+        marginLeft: 10,
+        fontSize: 16,
+        
+    },
+    border: {
+        borderBottomWidth: 2,
+        borderColor: "#a8b0de",   
+        paddingVertical: 0,
+        marginVertical: -5,  
+    },
+    margin: {
+        marginHorizontal: 15,
+    },
+    sistema: {
+        backgroundColor: "#aeb5df",
+        paddingLeft: 10,
+        fontSize: 16,
+        fontWeight: "bold",
+        paddingVertical: 7,
+    },
+    delete: {
+        color:"#f1c484",
+    },
+    icon: {
+        marginLeft: 30
+    },
+    itemContainer: {
+        flexDirection:'row', 
+        flexWrap:'wrap',
+        marginTop: 8,
+        paddingVertical: 1,
+    }
     
 });
--- frontend/src/screens/Settings.js
+++ frontend/src/screens/Settings.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View, Image } from "react-native";
 
 
-class Home extends Component {
+class Settings extends Component {
     render() {
         return (
-            <View style={{alignItems:"center", backgroundColor: "#5C6BC0",flex:1}}>
+            <View style={{alignItems:"center", backgroundColor: "#5C6BC0", flex:1}}>
              <Image source={require("../../assets/imgs/logo.png")}
         style={{ width: 150,borderBottomWidth: 5,
             height: 80 }}/>
-                <Text style={{fontSize: 30, color:"white"}}>Home</Text>           
+                <Text style={{fontSize: 30, color:"white"}}>Settings</Text>           
             </View>
         );
     }
 }
 
-export default Home;
+export default Settings;
