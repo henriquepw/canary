7e46cfce64b2553816447187bf3b84760926eb9d
Henrique Martins <enrque.ns@gmail.com>
2019-01-27 13:29:44 -0300

    scheduled task to insert daily readings

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,33 @@ const app = require("express")();
 const consign = require("consign");
 const db = require("./config/db");
 
+const CronJob = require("cron").CronJob;
+
+const { log } = console;
+
+const insertDaily = async () => {
+    const readings = await db("tb_canary").select(
+        "nh3",
+        "co2",
+        "co",
+        "temperature",
+        "humity",
+        "id as canary_id"
+    );
+
+    readings.forEach(element => {
+        log(element);
+    });
+
+    db("tb_daily_reading")
+        .insert(readings)
+        .then(_ => log("then"));
+};
+
+const job = new CronJob("*/20 * * * * *", insertDaily);
+
+//job.start();
+
 PORT = 3000;
 app.db = db;
 
@@ -14,5 +41,5 @@ consign()
     .into(app);
 
 app.listen(PORT, () => {
-    console.log(`Canary server started on port ${PORT}`);
+    log(`Canary server started on port ${PORT}`);
 });
